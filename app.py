b968c90e21248ba72dfb6db49ae00e6454ec4b71
Dauren Polatbekov <dauren.polatbekov+github@gmail.com>
2023-05-22 13:47:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,57 @@
 import streamlit as st
+import numpy as np
+from PIL import Image
 
+# Function to preprocess the input image
+def preprocess_image(image):
+    # Resize the image to the desired input shape (e.g., 5000x5000)
+    resized_image = image.resize((5000, 5000))
+    # Perform any other preprocessing steps if needed
+    # ...
+    return resized_image
+
+# Function to predict the segmentation mask using your trained UNet model
+def predict_mask(image):
+    # Perform any necessary preprocessing on the image
+    preprocessed_image = preprocess_image(image)
+    
+    # Convert the image to an array and normalize the RGB values
+    image_array = np.array(preprocessed_image) / 255.0
+    
+    # Use your trained UNet model to predict the segmentation mask
+    # ...
+    # Replace the following line with your actual prediction code
+    predicted_mask = np.random.randint(0, 2, size=(5000, 5000))
+    
+    return predicted_mask
+
+# Streamlit app code
 def main():
-    st.title("Welcome to My Streamlit App")
-    name = st.text_input("Enter your name:")
-    if st.button("Submit"):
-        if name:
-            st.success(f"Hello, {name}!")
-        else:
-            st.warning("Please enter your name.")
+    st.title("Rooftop Segmentation App")
+
+    # Upload image
+    uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+    if uploaded_image is not None:
+        # Display the uploaded image
+        image = Image.open(uploaded_image)
+        st.image(image, caption="Uploaded Image", use_column_width=True)
+
+        # Perform prediction and get the segmentation mask
+        predicted_mask = predict_mask(image)
+
+        # Display the segmentation mask
+        st.subheader("Segmentation Mask")
+        st.image(predicted_mask, caption="Segmentation Mask", use_column_width=True)
+
+        # Calculate and display the metrics (IoU, accuracy)
+        # ...
+        # Replace the following lines with your actual metric calculations
+        iou = np.random.rand()
+        accuracy = np.random.rand()
+        st.subheader("Metrics")
+        st.write("IoU:", iou)
+        st.write("Accuracy:", accuracy)
 
 if __name__ == "__main__":
     main()
