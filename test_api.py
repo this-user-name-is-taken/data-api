e6b37aac6635791e1dcc806417c684b2ae622698
Dauren Polatbekov <dauren.polatbekov+github@gmail.com>
2023-05-22 13:15:48 -0500

    Add files via upload

---
+++ test_api.py
@@ -0,0 +1,8 @@
+'''something'''
+
+import requests
+
+url = "https://weather.lewagon.com/geo/1.0/direct?q=Barcelona"
+response = requests.get(url).json()
+city = response[0]
+print(f"{city['name']}: ({city['lat']}, {city['lon']})")
