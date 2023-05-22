1d0587a625f1956507ba312ca0096a3ac1ead2ca
Dauren Polatbekov <dauren.polatbekov+github@gmail.com>
2023-05-22 22:10:24 +0400

    st_app

--- test_api.py
+++ test_api.py
@@ -1,4 +1,8 @@
-# pylint: disable=missing-docstring,invalid-name
+'''something'''
 
-# TODO: paste the code from Kitt's instructions
+import requests
 
+url = "https://weather.lewagon.com/geo/1.0/direct?q=Barcelona"
+response = requests.get(url).json()
+city = response[0]
+print(f"{city['name']}: ({city['lat']}, {city['lon']})")
--- weather.py
+++ weather.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-module-docstring
+'''info'''
 
 import sys
 import urllib.parse
@@ -12,19 +12,46 @@ def search_city(query):
     Look for a given city. If multiple options are returned, have the user choose between them.
     Return one city (or None)
     '''
-    pass  # YOUR CODE HERE
+    url = f"{BASE_URI}/geo/1.0/direct?q={urllib.parse.quote(query)}"
+    response = requests.get(url).json()
+
+    if not response:
+        print("Sorry, no cities found for that query.")
+        return None
+    elif len(response) == 1:
+        return response[0]
+    else:
+        print("Multiple cities found. Choose one:")
+        for i, city in enumerate(response):
+            print(f"{i+1}. {city['name']}, {city['country']}")
+        while True:
+            choice = input("> ")
+            if not choice.isdigit() or int(choice) < 1 or int(choice) > len(response):
+                print("Invalid choice. Please enter a number between 1 and", len(response))
+            else:
+                return response[int(choice)-1]
+print(response)
 
 def weather_forecast(lat, lon):
     '''Return a 5-day weather forecast for the city, given its latitude and longitude.'''
-    pass  # YOUR CODE HERE
+    url = f"{BASE_URI}/data/2.5/forecast?lat={lat}&lon={lon}"
+    response = requests.get(url).json()
+    return response['forecast']
 
 def main():
     '''Ask user for a city and display weather forecast'''
     query = input("City?\n> ")
     city = search_city(query)
 
-    # TODO: Display weather forecast for a given city
-    pass  # YOUR CODE HERE
+    if city:
+            print(f"Weather forecast for {city['name']}, {city['country']}:")
+
+            forecast = weather_forecast(city['lat'], city['lon'])
+
+            for day in forecast:
+                date = datetime.datetime.fromisoformat(day['dt_txt']).strftime('%A %d %B %Y')
+                print(f"{date}: {day['weather']}")
+                break
 
 if __name__ == '__main__':
     try:
