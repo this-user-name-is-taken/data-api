bbc9ef683f1a928180568533f3a7b9a0facd4ce9
Dauren Polatbekov <dauren.polatbekov+github@gmail.com>
2023-05-22 22:22:30 +0400

    1

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 def main():
-    st.title("Welcome to My Streamlit App")
-    name = st.text_input("Enter your name:")
+    st.title("Welcome to test Streamlit app")
+    name = st.text_input("First, enter your name (you can enter a fake name):")
     if st.button("Submit"):
         if name:
-            st.success(f"Hello, {name}!")
+            st.success(f"Hello, {name}! Streamlit app works OK with LeWagon requirements.txt but doesn't work with out requirements.txt:(")
         else:
             st.warning("Please enter your name.")
 
